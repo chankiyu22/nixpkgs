b1b832bc3dce163e6b3debed29bbe203bcd03e33
Ken Chan <chankiyu22@gmail.com>
2023-02-01 11:53:58 +0800

    Add lualine

--- neovim.nix
+++ neovim.nix
@@ -95,6 +95,7 @@ in
       lspkind-nvim
     ] ++ [
       # theme
+      lualine-nvim
       tokyonight-nvim
     ];
   };
---
+++ neovim/plugins/lualine.lua
@@ -0,0 +1,13 @@
+require("lualine").setup({
+	options = {
+		theme = "tokyonight",
+	},
+	sections = {
+		lualine_c = {
+			{
+				"filename",
+				path = 1,
+			},
+		},
+	},
+})
